aaa1c7d2f8273e77a366663f345c51a26e9228e3
Dane Springmeyer <dane@dbsgeo.com>
2011-06-13 00:40:11 -0400

    touchup test

--- test.js
+++ test.js
@@ -2,15 +2,20 @@
 
 var arc = require('./arc.js');
 
-var seattle = new arc.Coord(-122, 48);
-var dc = new arc.Coord(-77, 39);
-var gc = new arc.GreatCircle(seattle, dc);
-var options = {endpoints: false, mercator: false};
-var route = gc.geoJSON(50, options);
-console.log(JSON.stringify(route));
+var features = [];
+var geojson = { 'type': 'FeatureCollection',
+                'features': features
+              };
 
-// antipode should throw!
-/*
-var seattle = new arc.Coord(-122, 48);
-var gc = new arc.GreatCircle(seattle, seattle.antipode());
-*/
+var routes  = [
+    [ new arc.Coord(-122, 48), new arc.Coord(-77, 39) ], // seattle to dc  
+    [ new arc.Coord(-122, 48), new arc.Coord(0, 51) ], // seattle to london  
+  ];
+
+routes.forEach(function(route,idx) {
+    var gc = new arc.GreatCircle(route[0],route[1]);
+    var line = gc.Arc(50);
+    features.push(line.json());
+});
+
+console.log(JSON.stringify(geojson));
