b227d60aba6b3cc6b93e180b93c1d3ec42a8689b
john gravois <jagravois@gmail.com>
2022-07-09 15:34:59 -0700

    use gh actions and bump tape

--- test/arc.test.js
+++ test/arc.test.js
@@ -64,12 +64,12 @@ var routes = [
 //{ x: 114.576416015625, y:-44.21370990970204},{x: -65.423583984375, y:-43.19716728250127}
 
 var arcs = [
-  {"properties":{"name":"Seattle to DC"},"geometries":[{"coords":[[-122,48.00000000000001],[-97.72808611752785,45.75368189927002],[-77,38.99999999999999]],"length":3}]},
-  {"properties":{"name":"Seattle to London"},"geometries":[{"coords":[[-122,48.00000000000001],[-64.16590091973099,67.47624207149578],[0,51]],"length":3}]},
-  {"properties":{"name":"crosses dateline 1"},"geometries":[{"coords":[[-75.9375,35.46066995149531],[-136.82303405489677,-10.367409282634164],[146.25,-43.06888777416961]],"length":3}]},
-  {"properties":{"name":"crosses dateline 2"},"geometries":[{"coords":[[145.54687500000003,48.45835188280866],[-157.28484118689477,8.44205355944752],[-112.5,-37.71859032558814]],"length":3}]},
-  {"properties":{"name":"south 1"},"geometries":[{"coords":[[-74.564208984375,-0.17578097424708533],[-140.44327137076033,-35.80108605508993],[137.779541015625,-22.755920681486387]],"length":3}]},
-  {"properties":{"name":"south 2"},"geometries":[{"coords":[[-66.829833984375,-18.812717856407765],[-146.78177837397814,-82.1795027896656],[118.795166015625,-20.79720143430698]],"length":3}]}
+  {"properties":{"name":"Seattle to DC"},"geometries":[{"coords":[[-122,48],[-97.72808611752787,45.75368189927002],[-77,38.99999999999999]],"length":3}]},
+  {"properties":{"name":"Seattle to London"},"geometries":[{"coords":[[-122,48],[-64.16590091973097,67.47624207149578],[0,50.99999999999999]],"length":3}]},
+  {"properties":{"name":"crosses dateline 1"},"geometries":[{"coords":[[-75.9375,35.460669951495305],[-136.82303405489677,-10.367409282634164],[146.25,-43.06888777416961]],"length":3}]},
+  {"properties":{"name":"crosses dateline 2"},"geometries":[{"coords":[[145.54687500000003,48.45835188280866],[-157.28484118689477,8.44205355944752],[-112.50000000000001,-37.71859032558814]],"length":3}]},
+  {"properties":{"name":"south 1"},"geometries":[{"coords":[[-74.564208984375,-0.1757809742470853],[-140.44327137076033,-35.80108605508993],[137.779541015625,-22.755920681486387]],"length":3}]},
+  {"properties":{"name":"south 2"},"geometries":[{"coords":[[-66.829833984375,-18.81271785640776],[-146.78177837397814,-82.1795027896656],[118.795166015625,-20.79720143430698]],"length":3}]}
 ];
 
 
@@ -77,8 +77,7 @@ test('Routes', function(t) {
     routes.forEach(function(route,idx) {
         var gc = new arc.GreatCircle(route[0], route[1], route[2]);
         var line = gc.Arc(3);
-        //console.log(JSON.stringify(line))
-        t.deepEqual(line,arcs[idx]);
+        t.deepEqual(JSON.stringify(line), JSON.stringify(arcs[idx]));
     });
     t.end();
 });
