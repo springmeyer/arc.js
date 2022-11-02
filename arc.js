d63e387a84b8ae93ea5872b10ce920cc044c2897
Kadeem <kd@macbook-pro.lan>
2022-11-02 13:52:16 -0400

    truncate coordinate decimals to 6 places and update tests

--- arc.js
+++ arc.js
@@ -10,6 +10,17 @@ var Coord = function(lon,lat) {
     this.y = D2R * lat;
 };
 
+const truncate = function(coords) {
+    // truncate coordinate decimals to 6 places
+    const PRECISION = 6; 
+
+    for (let i = 0; i < coords.length; i++) {
+        coords[i] = Math.trunc(coords[i] * Math.pow(10, PRECISION)) / Math.pow(10, PRECISION);
+    }
+
+    return coords;
+}
+
 Coord.prototype.view = function() {
     return String(this.lon).slice(0, 4) + ',' + String(this.lat).slice(0, 4);
 };
@@ -240,7 +251,7 @@ GreatCircle.prototype.Arc = function(npoints,options) {
         arc.geometries.push(line);
         var points = poMulti[m];
         for (var j0 = 0; j0 < points.length; ++j0) {
-            line.move_to(points[j0]);
+            line.move_to(truncate(points[j0]));
         }
     }
     return arc;
